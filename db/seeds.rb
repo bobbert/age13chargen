259f2e338b73f747d16226c11f96c91a94beda43
Robert Phelps <bobbert0476@gmail.com>
2014-07-23 01:08:34 -0400

    fixed errors by converting methods to lambdas

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+
 srd_path = Dir.glob(Rails.root.to_s + "/db/srd_core.json").first
 srd = JSON.parse(open(srd_path).read, :symbolize_names=>true)
 srd_lookup = {}
 reverse_lookup = {}
+
+# creating defaults
+top_keys = [:tiers, :ability_scores, :point_buys, :levels, :races, :character_classes, :powers, :talents, :feats]
+top_keys.each do |top_key| 
+  srd[top_keys] ||= []
+  srd_lookup[top_key] = {}
+end
 
 fields = {
   tiers: [:name, :multiplier], 
@@ -25,19 +34,12 @@ fields = {
   feats: [:name, :description, :prereq]
 }
 
-# creating defaults
-top_keys = [:tiers, :ability_scores, :point_buys, :levels, :races, :character_classes, :powers, :talents, :feats]
-top_keys.each do |top_key| 
-  srd[top_keys] ||= []
-  srd_lookup[top_key] = {}
-end
-
-def assign_to_reverse_lookup(type, key, value)
+assign_to_reverse_lookup = lambda do |type, key, value|
   reverse_lookup[type] ||= {}
   reverse_lookup[type][key] = value
 end
 
-def create_and_assign_to_srd_lookup(clazz, clazz_obj, key_field)
+create_and_assign_to_srd_lookup = lambda do |clazz, clazz_obj, key_field|
   record_name = clazz.to_s.underscore.pluralize.to_sym
   if (fields[record_name])
     # creating new ActiveModel record with only valid fields
@@ -63,36 +65,36 @@ end
 # Parent records (CharacterClass, Race) get persisted to Mongo.
 
 srd[:tiers].each do |tier_obj|
-  create_and_assign_to_srd_lookup(Tier, tier_obj, :name)
+  create_and_assign_to_srd_lookup.call(Tier, tier_obj, :name)
 end
 
 srd[:levels].each do |level_obj|
   level_obj[:tier] = srd_lookup[:tiers][level_obj[:tier]] if level_obj[:tier] # Level -> Tier
-  create_and_assign_to_srd_lookup(Level, level_obj, :level)
+  create_and_assign_to_srd_lookup.call(Level, level_obj, :level)
 end
 
 srd[:feats].each do |feat_obj|
   feat_obj[:tier] = srd_lookup[:tiers][feat_obj[:tier]] if feat_obj[:tier] # Feat -> Tier
-  create_and_assign_to_srd_lookup(Feat, feat_obj, :name)
+  create_and_assign_to_srd_lookup.call(Feat, feat_obj, :name)
 end
 
 srd[:talents].each do |talent_obj|
   talent_obj[:tier] = srd_lookup[:talents][talent_obj[:tier]] if talent_obj[:tier] # Talent -> Tier
-  create_and_assign_to_srd_lookup(Talent, talent_obj, :name)
+  create_and_assign_to_srd_lookup.call(Talent, talent_obj, :name)
 end
 
 srd[:powers].each do |power_obj|
   power_obj[:level] = srd_lookup[:powers][power_obj[:level]] if power_obj[:level] # Power -> Level
 #  power_obj[:feat] = srd_lookup[:powers][power_obj[:feat]] if power_obj[:feat] # Power -> Feat
-  create_and_assign_to_srd_lookup(Power, power_obj, :name)
+  create_and_assign_to_srd_lookup.call(Power, power_obj, :name)
 end
 
 srd[:races].each do |race_obj|
-  create_and_assign_to_srd_lookup(Race, race_obj, :name)
+  create_and_assign_to_srd_lookup.call(Race, race_obj, :name)
 end
 
 srd[:character_classes].each do |character_class_obj|
-  create_and_assign_to_srd_lookup(CharacterClass, character_class_obj, :name)
+  create_and_assign_to_srd_lookup.call(CharacterClass, character_class_obj, :name)
 end
 
 # performing child-parent associations
